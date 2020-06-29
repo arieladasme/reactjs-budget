a693bd5aa716eb9e0074ad16365da468c14b5193
Ariel <ariel.wm1@gmail.com>
2020-06-29 17:54:20 -0400

    Reading budget

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,33 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Question = () => {
+  const [quantity, saveQuantity] = useState(0)
+
+  // Read budget
+  const defineBudget = e => {
+    saveQuantity(parseInt(e.target.value, 10))
+  }
+
+  // Submit
+  const addBudget = e => {
+    e.preventDefault()
+  }
+
   return (
     <Fragment>
       <h2>Enter your budget</h2>
 
-      <form>
-        <input type="number" className="u-full-width" placeholder="Budget" />
+      <form onSubmit={addBudget}>
+        <input
+          type="number"
+          className="u-full-width"
+          placeholder="Budget"
+          onChange={defineBudget}
+        />
         <input
           type="submit"
           className="u-full-width button-primary"
-          value="Define budfget"
+          value="Define budget"
         />
       </form>
     </Fragment>

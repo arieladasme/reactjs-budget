6276e66d767bae9f261efda73eba9e18e307f9c8
Ariel <ariel.wm1@gmail.com>
2020-06-29 18:06:34 -0400

    Validate budget entry

---
+++ src/components/Error.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Error = ({ message }) => <p className="alert alert-danger">{message}</p>
+
+export default Error
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,7 +1,9 @@
 import React, { Fragment, useState } from 'react'
+import Error from './Error'
 
 const Question = () => {
   const [quantity, saveQuantity] = useState(0)
+  const [error, saveError] = useState(false)
 
   // Read budget
   const defineBudget = e => {
@@ -11,12 +13,20 @@ const Question = () => {
   // Submit
   const addBudget = e => {
     e.preventDefault()
+
+    // Validate budget entry
+    if (quantity < 1 || isNaN(quantity)) {
+      saveError(true)
+      return
+    }
+
+    saveError(false)
   }
 
   return (
     <Fragment>
       <h2>Enter your budget</h2>
-
+      {error ? <Error message="Te budget is wrong" /> : null}
       <form onSubmit={addBudget}>
         <input
           type="number"
